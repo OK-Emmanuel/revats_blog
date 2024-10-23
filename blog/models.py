397d39f37d90b67576cb7210d2e3d0be5675c23d
OK-Emmanuel <olawuniemmanuelk@ggmail.com>
2024-10-23 11:50:41 +0100

    Created Author model to be associated with each blog post

--- blog/models.py
+++ blog/models.py
@@ -97,3 +97,23 @@ class BlogPageGalleryImage(Orderable):
         FieldPanel('caption'),
     ]
 
+
+# Author model (Snippet)
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('author_image'),
+    ]
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Authors'
+
