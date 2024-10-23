87a806e4510b135ad457fa8f36eeab47e007bb97
OK-Emmanuel <olawuniemmanuelk@ggmail.com>
2024-10-23 11:44:52 +0100

    imported required modules for blog app, tagging, fieldpanels and richtextfield

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django import forms
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.snippets.models import register_snippet
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.search import index
+
+
