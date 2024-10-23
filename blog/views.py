4b74b46de57b4aaa44075b4dc3aba213caa9a379
OK-Emmanuel <olawuniemmanuelk@ggmail.com>
2024-10-23 11:43:06 +0100

    Blog app and functionalities

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+# from django.shortcuts import render, redirect
+# from wagtail.models import Page
+# # Create your views here.
+# def blog_index_redirect(request):
+#     # Find the blog index page and redirect
+#     blog_index_page = Page.objects.get(slug='blog')
+#     return redirect(blog_index_page.url)
